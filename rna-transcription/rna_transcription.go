ae200b19fd28c7aaa95920cf2b64d95d73a5cc04
Shid Insa <theshid.insa@gmail.com>
2021-03-10 01:00:31 +0100

    commit

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -4,15 +4,25 @@ package strand
 func ToRNA(dna string) string {
 	rna := ""
 
-	for i:=0;i<len(dna);i++{
-       rna+= string(transcription[dna[i]])
+	for i := 0; i < len(dna); i++ {
+		rna += string(transcription[dna[i]])
 	}
 	return rna
 }
 
 var transcription = [256]byte{
-	'G' :'C',
-	'C' :'G',
-	'T' :'A',
-	'A' :'U',
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+
+//Best solution
+	/*func ToRna(input string) string{
+	return strings.NewReplacer(
+	"G", "C",
+	"C", "G",
+	"T", "A",
+	"A", "U",
+).Replace(input)
+}*/
 }
