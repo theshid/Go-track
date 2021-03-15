b8817831a221ff905505ee18d0a4415688f38dc4
Shid Insa <theshid.insa@gmail.com>
2021-03-15 11:30:51 +0100

    commit

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -6,15 +6,14 @@ import (
 
 //IsIsogram is a function that determine if a given string have duplicate characters excluding hyphen and empty space
 func IsIsogram(word string) bool {
-	wordInRune := []rune(strings.ToLower(word))
-	hyphenAndEmptySpace := []rune("- ")
+
+	replacer := strings.NewReplacer("-", "", " ", "")
+	trimWord := replacer.Replace(strings.ToLower(word))
+
+	wordInRune := []rune(trimWord)
 	stringMap := make(map[rune]int)
-	for i := 0; i < len(word); i++ {
 
-		if wordInRune[i] == hyphenAndEmptySpace[0] || wordInRune[i] == hyphenAndEmptySpace[1] {
-			continue
-		}
-
+	for i := 0; i < len(trimWord); i++ {
 		stringMap[wordInRune[i]]++
 		if stringMap[wordInRune[i]] > 1 {
 			return false
