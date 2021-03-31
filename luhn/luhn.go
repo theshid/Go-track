ad942055e0b8a5ac572f4dd5f0b67c30eecba968
Shid Insa <theshid.insa@gmail.com>
2021-03-31 11:00:37 +0100

    commit

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,33 +2,30 @@ package luhn
 
 import (
 	"strings"
-	"unicode"
 )
 
 // Valid check if the given string satisfies the luhn algorithm
 func Valid(input string) bool {
-	trimInput := []rune(strings.ReplaceAll(input, " ", ""))
+	trimInput := strings.ReplaceAll(input, " ", "")
 	count := 0
 	if len(trimInput) <= 1 {
 		return false
 	}
-	isSecondDigit := false
-	for i := len(trimInput) - 1; i >= 0; i-- {
-		value := trimInput[i]
-		if !unicode.IsDigit(value) {
+	checkLengthParity := len(trimInput) % 2
+
+	for i, r := range trimInput {
+		digit := int(r - '0')
+		if digit < 0 || digit > 9 {
 			return false
 		}
-		digit := int(value - '0')
-		if digit >= 0 && digit <= 9 {
-			if isSecondDigit {
-				digit = digit * 2
-				if digit > 9 {
-					digit -= 9
-				}
+		if i%2 == checkLengthParity {
+			digit = digit * 2
+			if digit > 9 {
+				digit -= 9
 			}
-			count += digit
-			isSecondDigit = !isSecondDigit
 		}
+		count += digit
+
 	}
 	return count%10 == 0
 }
