9136100bd0aa875ba0db8388494f2824289087e6
Shid Insa <theshid.insa@gmail.com>
2021-03-31 19:48:55 +0100

    commit

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -6,25 +6,26 @@ import (
 
 // Valid check if the given string satisfies the luhn algorithm
 func Valid(input string) bool {
-	trimInput := strings.ReplaceAll(input, " ", "")
+	input = strings.ReplaceAll(input, " ", "")
 	count := 0
-	if len(trimInput) <= 1 {
+	if len(input) <= 1 {
 		return false
 	}
-	checkLengthParity := len(trimInput) % 2
+	double := len(input)%2 == 0
 
-	for i, r := range trimInput {
+	for _, r := range input {
 		digit := int(r - '0')
 		if digit < 0 || digit > 9 {
 			return false
 		}
-		if i%2 == checkLengthParity {
+		if double {
 			digit = digit * 2
 			if digit > 9 {
 				digit -= 9
 			}
 		}
 		count += digit
+		double = !double
 
 	}
 	return count%10 == 0
